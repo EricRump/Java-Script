cb0660a400937be7fb08e2f1cdfbd753da3d7510
Eric <rumperic@gmail.com>
2024-02-14 15:50:25 +0100

    erste aufgabe

--- Hello World/script.js
+++ Hello World/script.js
@@ -1,3 +1,21 @@
 console.log("Hello World");
-console.log(3 - 2);
-console.log(45 * 12);
+// arithmetic operator
+console.log(3 - 2); //output 1
+console.log(45 * 12); // output 540
+
+// comparison operator
+console.log(1 === 1); // true
+console.log(1 !== 1); //false
+
+//logical operator
+console.log(2 < 3 && 5 > 3); // true and operator
+console.log(2 > 1 || 2 > 3); // true or operator
+console.log(!(5 > 6)); // true not operator
+
+// grouping operator
+console.log(7 + 20 / 5); // 11
+console.log((7 + 20) / 5); //5.4
+console.log(5 * 3 === 5 * 4); // false
+
+// concatenation operator
+console.log("Hallo" + "ich bin" + "Eric"); // Hallo ich bin Eric
