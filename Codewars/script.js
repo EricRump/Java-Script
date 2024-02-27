90ff046f300cb5ed65bc461481fb67d0d9aad306
Eric <rumperic@gmail.com>
2024-02-27 14:29:37 +0100

    alphabet war

--- Codewars/script.js
+++ Codewars/script.js
@@ -478,3 +478,49 @@ function whosOnline(friends) {
   }
   return result;
 }
+
+//Alphabet war
+
+function alphabetWar(fight) {
+  let sum = 0;
+  for (i = 0; i < fight.length; i++) {
+    let char = fight[i];
+    switch (char) {
+      case "m":
+        sum += 4;
+        break;
+      case "q":
+        sum += 3;
+        break;
+      case "d":
+        sum += 2;
+        break;
+      case "z":
+        sum += 1;
+        break;
+      case "w":
+        sum -= 4;
+        break;
+      case "p":
+        sum -= 3;
+        break;
+      case "b":
+        sum -= 2;
+        break;
+      case "s":
+        sum -= 1;
+        break;
+      default:
+        break;
+    }
+  }
+  if (sum < 0) {
+    return "Left side wins!";
+  }
+  if (sum > 0) {
+    return "Right side wins!";
+  }
+  if (sum === 0) {
+    return "Let's fight again!";
+  }
+}
--- Codewars/üben/script.js
+++ Codewars/üben/script.js
@@ -1,19 +1,44 @@
-function killer(suspectInfo, dead) {
-  for (let i = 0; i < suspectInfo.length; i++) {
-    let subArray = suspectInfo[i];
-    for (let j = 0; j < subArray.length; j++) {
-      let element = subArray[j];
-      if (element === dead) {
-        return suspectInfo[i];
-      }
+function alphabetWar(fight) {
+  let sum = 0;
+  for (i = 0; i < fight.length; i++) {
+    let char = fight[i];
+    switch (char) {
+      case "m":
+        sum += 4;
+        break;
+      case "q":
+        sum += 3;
+        break;
+      case "d":
+        sum += 2;
+        break;
+      case "z":
+        sum += 1;
+        break;
+      case "w":
+        sum -= 4;
+        break;
+      case "p":
+        sum -= 3;
+        break;
+      case "b":
+        sum -= 2;
+        break;
+      case "s":
+        sum -= 1;
+        break;
+      default:
+        break;
+    }
+    if (sum < 0) {
+      return "Left side wins!";
+    }
+    if (sum > 0) {
+      return "Right side wins!";
+    }
+    if (sum === 0) {
+      return "Let's fight again!";
     }
   }
 }
-console.log(
-  killer(
-    "James"[("Jacob", "Bill", "Lucas")],
-    "Johnny"[("David", "Kyle", "Lucas")],
-    "Peter"[("Lucy", "Kyle")],
-    ["Lucas", "Bill"]
-  )
-);
+console.log(alphabetWar(m));
